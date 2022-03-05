9b177d944d01588002c244f1d6c94fcaed9b715d
ShirleyyFang <yanbingfang@utexas.edu>
2022-03-04 23:15:46 -0800

    Update books.js

--- my-app/src/books.js
+++ my-app/src/books.js
@@ -1,66 +1,84 @@
 export const books = [
     {
         id:1,
+        name:'刀锋',
+        author: '威廉·萨默赛特·毛姆',
+        imgURL:"https://images-cn.ssl-images-amazon.cn/images/I/412OB7vQnoL.jpg"
+    },
+    {
+        id:2,
+        name:'大流感:最致命瘟疫的史诗',
+        author:'约翰·M·巴里',
+        imgURL:"https://www.gpdf.net/wp-content/uploads/2021/08/20479197-1_u.jpg"
+    },
+    {
+        id:3,
+        name:'在路上',
+        author:'杰克·凯鲁亚克',
+        imgURL:"https://imagepphcloud.thepaper.cn/pph/image/66/254/29.jpg"
+    },
+    {
+        id:4,
         name:'斯通纳',
         author:'约翰·威廉斯',
         imgURL:"http://img1.gtimg.com/cul/pics/hv1/218/26/1990/129406598.jpg"     
     },
     {
-        id:2,
+        id:5,
         name:'那些忧伤的年轻人',
         author:'F.S.菲茨杰拉德',
         imgURL:"https://m.media-amazon.com/images/I/51TTchQqRFL.jpg"   
     },
     {
-        id:3,
+        id:6,
         name:'夜色温柔',
         author:'F.S.菲茨杰拉德',
         imgURL:"https://m.media-amazon.com/images/I/41dX1jjL3ML.jpg"   
     },
     {
-        id:4,
+        id:7,
         name:'教父',
         author:'马里奥·普佐',
         imgURL:"https://cdn.britannica.com/45/171445-050-636F0802/Book-jacket-The-Godfather-Mario-Puzo.jpg"   
     },
     {
-        id:5,
+        id:8,
         name:'一句顶一万句',
         author:'刘震云',
         imgURL:"https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1386228512l/19200550.jpg"   
     },
     {
-        id:6,
+        id:9,
         name:'罪与罚',
         author:'陀思妥耶夫斯基',
         imgURL:"https://m.media-amazon.com/images/I/51qyn0XNwPL.jpg"   
     },
     {
-        id:7,
+        id:10,
         name:'约翰·克利斯朵夫',
         author:'罗曼·罗兰',
         imgURL:"https://img.pdf100.net/front_cover/5380561486025404092172.jpg"
     },
     {
-        id:8,
+        id:11,
         name:'棋王',
         author:'阿城',
         imgURL:"https://www.stortiktok.com/wp-content/uploads/2021/05/1621952388-672fa6595b65b24.jpg"   
     },
     {
-        id:9,
+        id:12,
         name:'围城',
         author:'钱钟书',
         imgURL:"https://images-cn.ssl-images-amazon.cn/images/I/51RMtQatx0L.jpg"   
     },
     {
-        id:10,
+        id:13,
         name:'离婚',
         author:'老舍',
         imgURL:"https://easyreadfs.nosdn.127.net/rThZe4i57zvzQ8Gi5yaFrg==/8796093024901813151"   
     },
     {
-        id:10,
+        id:14,
         name:'球状闪电',
         author:'刘慈欣',
         imgURL:"http://puui.qpic.cn/vcover_vt_pic/0/pj1003701sklybl/0"
