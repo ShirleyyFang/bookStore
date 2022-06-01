c57711952a980ad917abfb65b6372165a1fbc0ff
ShirleyyFang <yanbingfang@utexas.edu>
2022-05-31 21:19:59 -0700

    Update books.js

--- my-app/src/books.js
+++ my-app/src/books.js
@@ -1,114 +1,126 @@
 export const books = [
     {
         id:1,
+        name:'漫长的告别',
+        author:'雷蒙德·钱德勒',
+        imgURL:'http://cover.read.duokan.com/mfsv2/download/s010/p01MjapV9dEp/JWKvXv8cOcm02f.jpg!l'
+    },
+    {
+        id:2,
+        name:'梁永安：阅读、游历和爱情',
+        author:'梁永安',
+        imgURL:'https://ebookcn.com/storage/upload/files/000/31/95/27_cover_m.jpg'
+    },
+    {
+        id:3,
         name:'那不勒斯四部曲',
         author:'埃莱娜·费兰特',
         imgURL:'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1590806524l/53642524._SY475_.jpg'
     },
     {
-        id:2,
+        id:4,
         name:'逃离',
         author:'爱丽丝·门罗',
         imgURL:'https://tva3.sinaimg.cn/large/007g2d70gy1gd7dyuvmx5j307i0am0sq.jpg'
     },
     {
-        id:3,
+        id:5,
         name:'达洛维夫人',
         author:'弗吉尼亚·伍尔芙',
         imgURL:"https://img3.bangli.uk/images/cms/2016/11/218445_1.jpg"
     },
     {
-        id:4,
+        id:6,
         name:'李诞脱口秀工作手册',
         author:'李诞',
         imgURL:"https://images-na.ssl-images-amazon.com/images/I/61nHpQ7pKkL.jpg"
     },
     {
-        id:5,
+        id:7,
         name:'刀锋',
         author: '威廉·萨默赛特·毛姆',
         imgURL:"https://images-cn.ssl-images-amazon.cn/images/I/412OB7vQnoL.jpg"
     },
     {
-        id:6,
+        id:8,
         name:'山月记',
         author: '中岛敦',
         imgURL:"https://m.media-amazon.com/images/I/418rS18ZRUL.jpg"
     },
     {
-        id:7,
+        id:9,
         name:'大流感:最致命瘟疫的史诗',
         author:'约翰·M·巴里',
         imgURL:"https://www.gpdf.net/wp-content/uploads/2021/08/20479197-1_u.jpg"
     },
     {
-        id:8,
+        id:10,
         name:'在路上',
         author:'杰克·凯鲁亚克',
         imgURL:"https://imagepphcloud.thepaper.cn/pph/image/66/254/29.jpg"
     },
     {
-        id:9,
+        id:11,
         name:'斯通纳',
         author:'约翰·威廉斯',
         imgURL:"http://img1.gtimg.com/cul/pics/hv1/218/26/1990/129406598.jpg"     
     },
     {
-        id:10,
+        id:12,
         name:'那些忧伤的年轻人',
         author:'F.S.菲茨杰拉德',
         imgURL:"https://m.media-amazon.com/images/I/51TTchQqRFL.jpg"   
     },
     {
-        id:11,
+        id:13,
         name:'夜色温柔',
         author:'F.S.菲茨杰拉德',
         imgURL:"https://m.media-amazon.com/images/I/41dX1jjL3ML.jpg"   
     },
     {
-        id:12,
+        id:14,
         name:'教父',
         author:'马里奥·普佐',
         imgURL:"https://cdn.britannica.com/45/171445-050-636F0802/Book-jacket-The-Godfather-Mario-Puzo.jpg"   
     },
     {
-        id:13,
+        id:15,
         name:'一句顶一万句',
         author:'刘震云',
         imgURL:"https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1386228512l/19200550.jpg"   
     },
     {
-        id:14,
+        id:16,
         name:'罪与罚',
         author:'陀思妥耶夫斯基',
         imgURL:"https://m.media-amazon.com/images/I/51qyn0XNwPL.jpg"   
     },
     {
-        id:15,
+        id:17,
         name:'约翰·克利斯朵夫',
         author:'罗曼·罗兰',
         imgURL:"http://cover.read.duokan.com/mfsv2/download/fdsc3/p01T5yEjwj4g/oJdRnLBCAGNxz4.jpg!l"
     },
     {
-        id:16,
+        id:18,
         name:'棋王',
         author:'阿城',
         imgURL:"https://www.stortiktok.com/wp-content/uploads/2021/05/1621952388-672fa6595b65b24.jpg"   
     },
     {
-        id:17,
+        id:19,
         name:'围城',
         author:'钱钟书',
         imgURL:"https://images-cn.ssl-images-amazon.cn/images/I/51RMtQatx0L.jpg"   
     },
     {
-        id:18,
+        id:20,
         name:'离婚',
         author:'老舍',
         imgURL:"https://easyreadfs.nosdn.127.net/rThZe4i57zvzQ8Gi5yaFrg==/8796093024901813151"   
     },
     {
-        id:19,
+        id:21,
         name:'球状闪电',
         author:'刘慈欣',
         imgURL:"http://puui.qpic.cn/vcover_vt_pic/0/pj1003701sklybl/0"
